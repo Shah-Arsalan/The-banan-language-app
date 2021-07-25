7d759909a9dcb20ad06817ab4721562e81c502e1
Shah-Arsalan <shaharslan2278@gmail.com>
2021-07-25 14:39:38 +0530

    Did till API part

---
+++ app.js
@@ -0,0 +1,10 @@
+var translatebutton = document.querySelector("#translate");
+var textbox = document.querySelector("#text");
+
+translatebutton.addEventListener("click", clickButton)
+
+function clickButton(){
+    console.log("clicked123");
+    console.log(textbox.value);
+};
+  
