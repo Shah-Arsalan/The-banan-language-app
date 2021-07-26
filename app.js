34849376c3410d3ce6e5315761b89bf8bdad44de
Shah-Arsalan <shaharslan2278@gmail.com>
2021-07-26 13:06:41 +0530

    Done with input , output and working

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var translatebutton = document.querySelector("#translate");
 var textbox = document.querySelector("#text");
+var outputbox = document.querySelector("#output-area");
 
-translatebutton.addEventListener("click", clickButton)
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function fullurl(inputtext){
+    return url + "?" + "text=" + inputtext
+}
+
+function erroroccured(error){
+    console.log("error occured", error);
+    alert("Something went wrong . Try again later")
+}
+
+
+
+
 
 function clickButton(){
-    console.log("clicked123");
-    console.log(textbox.value);
+  var textentered = textbox.value;
+  fetch(fullurl(textentered))
+  .then(response => response.json())
+  .then(json => {
+      var translatedoutput = json.contents.translated;
+      outputbox.innertext = translatedoutput;
+  })
+  .catch(erroroccured)
 };
   
+translatebutton.addEventListener("click", clickButton)
